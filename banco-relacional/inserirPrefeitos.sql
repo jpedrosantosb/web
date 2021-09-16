42b26081d9c888792082680f9ebb58cc807ff6c5
João Pedro <jpedrosantosb@gmail.com>
2021-09-15 21:50:59 -0300

    tabela prefeitos
    criação e inserção na tabela prefeitos & consultas com diversos tipos de joins

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,9 @@
+select * from cidades;
+
+INSERT INTO prefeitos (nome, cidade_id) VALUES ('Joãozinho', 5), ('Raquel Lyra', 4), ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rafael Greca', null);
+
+INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rodrigo Pinheiro', 3);
