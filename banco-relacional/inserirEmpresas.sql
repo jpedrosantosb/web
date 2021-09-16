fb70063d2ca2642b911c384b63d94a86910d8ff1
João Pedro <jpedrosantosb@gmail.com>
2021-09-15 22:31:51 -0300

    tabela empresas

---
+++ banco-relacional/inserirEmpresas.sql
@@ -0,0 +1,13 @@
+ALTER TABLE empresas MODIFY cnpj VARCHAR(14);
+
+INSERT INTO empresas (nome, cnpj) VALUES ('Bradesco', 95694186000132), ('Vale', 27887148000146), ('Cielo', 01598317000134);
+
+
+desc empresas;
+desc prefeitos;
+select * from empresas;
+select * from cidades;
+
+INSERT INTO empresas_unidades (empresa_id, cidade_id, sede) VALUES (1, 1, 1), (1, 3, 0), (3, 1, 0), (2, 4, 1);
+
+select * from empresas_unidades;
